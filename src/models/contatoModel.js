a05c3b24d9c462d197113a1a28fb57e24e432995
Paulo Robert Lima Gomes <paulorobert5776@gmail.com>
2025-05-22 21:04:27 -0300

    feat: modelo de schema para a entidade contato

---
+++ src/models/contatoModel.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const contatoSchema = mongoose.Schema({
+    nome: {
+        type: String,
+        required: true,
+    },
+    sobrenome: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        lowercase: true,
+        unique: true,
+    },
+    telefone: {
+        type: String,
+        required: true,
+        minlength: 11
+    }
+})
+
+const ContatoModel = mongoose.model('Contato', contatoSchema);
+
+module.exports = ContatoModel;
