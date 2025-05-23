a62c0518a7f02dcfe0b668261e515a446d680f1a
Paulo Robert Lima Gomes <paulorobert5776@gmail.com>
2025-05-22 23:03:13 -0300

    feat: utilizacao de views e reconfiguracao no styles no webpack

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const router = require('./route');
+const path = require('path');
 const app = express();
 
+app.set('views', path.resolve(__dirname, 'src', 'views'));
+app.set('view engine', 'ejs');
 app.use(router);
 
 module.exports = app;
