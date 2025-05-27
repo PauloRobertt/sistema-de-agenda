bdcd3f78add9fa0d535c086eaa8f11f476e785de
Paulo Robert Lima Gomes <paulorobert5776@gmail.com>
2025-05-27 18:49:52 -0300

    feat: implementacao da pagina para realizar a criacao de contato, modificacao no style e utilizacao do express(json)

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const router = require('./src/routes/route.js');;
 const path = require('path');
 const app = express();
 
+app.use(express.json());
 app.use(express.static(path.resolve(__dirname, 'public')));
 app.set('views', path.resolve(__dirname, 'src', 'views'));
 app.set('view engine', 'ejs');
--- src/services/contatoService.js
+++ src/services/contatoService.js
@@ -9,5 +9,35 @@ module.exports = {
         return await contatoModel.find({});
     },
 
-    findById() { }
+    async findById(id) {
+        if (!id) throw new Error('ID é obrigatorio!');
+
+        return await contatoModel.findById(id);
+    },
+
+    async createContato(data) {
+        try {
+            const { nome, sobrenome, email, telefone } = data;
+
+            if (!nome || !sobrenome || !email || !telefone) throw new Error('Todos os campos são obrigatorios!');
+            
+            if(telefone.length < 11) throw new Error('Campo telefone precisa ter no minimo 11 caracteres');
+
+            const emailExistente = await contatoModel.find({ email: email }).exec();
+
+            if (emailExistente) throw new Error('Email já em uso!');
+
+
+            const newContato = new contatoModel({
+                nome,
+                sobrenome,
+                email,
+                telefone
+            })
+
+            return await newContato.save();
+        } catch (error) {
+            console.log(error);
+        }
+    }
 }
