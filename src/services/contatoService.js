773fa9f55a387af53d45c21fa2c7fde6a02f28a8
Paulo Robert Lima Gomes <paulorobert5776@gmail.com>
2025-05-27 22:24:53 -0300

    feat: implementacao do edit para contatos

--- src/services/contatoService.js
+++ src/services/contatoService.js
@@ -40,6 +40,35 @@ module.exports = {
         }
     },
 
+    async findByIdAndUpdate(data, id){
+        try {
+            const { nome, sobrenome, email, telefone } = data;
+
+            if(!id) throw new Error('ID é obrigatorio!');
+            
+            const contatoExistente = await contatoModel.find({ _id: id });
+
+            if(contatoExistente.length === 0) throw new Error('Contato não existe!');
+
+            if (!nome || !sobrenome || !email || !telefone) throw new Error('Todos os campos são obrigatorios!');
+
+            if (telefone.length < 11) throw new Error('Campo telefone precisa ter no minimo 11 caracteres');
+
+            const emailExistente = await contatoModel.find({ email: email }).exec();
+
+            if(emailExistente.length != 0) throw new Error('Email já em uso!');
+
+            return await contatoModel.findByIdAndUpdate(id, {
+                nome: nome,
+                sobrenome: sobrenome,
+                email: email,
+                telefone: telefone
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    },
+
     async findByIdAndDelete(id) {
         try {
             if (!id) throw new Error('ID é obrigatorio!');
