c0bb4d091f04287d9936c6aa0e8d5d6d06d0e9ae
Paulo Robert Lima Gomes <paulorobert5776@gmail.com>
2025-05-30 19:10:13 -0300

    chore: implementacao das rotas

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const router = express.Router();
 
-const { telaHome } = require('../controllers/homeController.js');
+const homeController = require('../controllers/homeController.js');
+const contatoController = require('../controllers/contatoController.js');
+const authController = require('../controllers/authController.js');
 
-router.get('/', telaHome);
+router.get('/', homeController.telaPrincipal);
+router.get('/contato/index', contatoController.index);
+router.get('/contato/edit/:id', contatoController.indexEditarContato);
+router.get('/auth/index', authController.index);
+
+router.get('/contato', contatoController.retornarContatos);
+router.get('/contato/:id', contatoController.retornarContato);
+router.post('/contato', contatoController.criarContato);
+router.put('/contato/:id', contatoController.editContato);
+router.delete('/contato/:id', contatoController.deletarContato);
+
+router.post('/auth/login', authController.login);
+router.post('/auth/register', authController.register);
 
 module.exports = router;
