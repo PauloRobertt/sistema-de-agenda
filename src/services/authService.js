e2e0c03acf5a46b877a0034c5b17f1a325f3a0ca
Paulo Robert Lima Gomes <paulorobert5776@gmail.com>
2025-06-04 17:51:39 -0300

    feat: validador de e-mail

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,4 +1,5 @@
 const usuarioModel = require('../models/usuarioModel.js');
+const validator = require('validator');
 
 module.exports = {
     login() { },
@@ -9,12 +10,13 @@ module.exports = {
 
             if (!email || !senha) throw new Error('Todos os campo são obrigatorios!');
 
+            if(!validator.isEmail(email)) throw new Error('E-mail invalido!')
+
             const emailExistente = await usuarioModel.find({ email: email });
 
             if (emailExistente.length != 0) throw new Error('E-mail já em uso!');
 
             const bcrypt = require('bcrypt');
-
             const saltRounds = 10;
             const salt = bcrypt.genSaltSync(saltRounds);
             const senhaHash = bcrypt.hashSync(senha, salt);
@@ -26,7 +28,7 @@ module.exports = {
 
             return await newUsuario.save();   
         } catch (error) {
-            console.log(error);
+            throw error;
         }
     }
 }
